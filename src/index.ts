5d720767d9e88502d62711c4da6bed9cdb0173a9
James Conkling <james.lane.conkling@gmail.com>
2019-04-28 19:44:53 -0400

    add markers

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import { select } from 'd3-selection'
 import * as topojson from 'topojson-client'
 // import world from './assets/world-110m.json'
 import land from './assets/ne_110m_land.json'
+// import eye from './assets/eye.png';
 
 
-// const svg = document.getElementById('map')
+
+const projection = geo.geoEquirectangular();
+
+// 40°39’30.6”N 73°58’03.7”W
+const videos = [
+  { lat: 40, lng: -73 }
+];
+
 const app = select('#app')
 
 app.append('h1')
@@ -24,26 +32,31 @@ svg.attr('viewBox', '0 0 960 600')
 
 svg.append("path")
   .datum({type: "FeatureCollection", features: land.features})
-  .attr("d", geo.geoPath().projection(geo.geoEquirectangular()) as any)
+  .attr("d", geo.geoPath().projection(projection) as any)
   // .attr("d", geo.geoPath().projection(geo.geoMercator()) as any)
   // .attr("d", geo.geoPath().projection(geo.geoNaturalEarth1()) as any)
   // .attr("d", geo.geoPath().projection(geo.geoEqualEarth()) as any)
-  .style('fill', 'white')
-  .style('stroke', '#000')
-  .style('stroke-width', '0.5px');
+  .style('stroke-width', '0.6px')
 
-// svg.selectAll("path")
-//   .data(land)
-//   .enter().append("path")
-//     .attr("d", geo.geoPath());
 
-// svg.append('path')
-//   .datum(land)
-//   .enter().append('path')
-//     .attr('d', geo.geoPath().projection(geo.geoMercator()))
+svg.append("g")
+  .selectAll("circle")
+  .data(videos)
+  .join("circle")
+    .attr("cx", d => projection([d.lng, d.lat])[0])
+    .attr("cy", d => projection([d.lng, d.lat])[1])
+    .attr("r", 3)
 
-// svg.selectAll("path")
-//   // .data(topojson.feature(world, world.objects.land).features)
-//   // .datum(topojson.mesh(world, world.objects.land))
-//   .enter().append("path")
-//   .attr("d", path);
+// const markerWidth = 20
+// const markerHeight = 20
+// svg.append("g")
+//   .selectAll("image")
+//   .data(videos)
+//   .join("image")
+//     .attr("xlink:href", eye)
+//     .attr("width", `${markerWidth}px`)
+//     .attr("height", `${markerHeight}px`)
+//     .attr("transform", d => {
+//       const [x, y] = projection([d.lng, d.lat])
+//       return `translate(${x - (markerHeight / 2)}, ${y - (markerWidth / 2)})`
+//     })
